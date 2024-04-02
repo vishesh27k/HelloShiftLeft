dfbeaeef4a5d59f02aed123e26c36bcb8875ba5c
Vishesh <45128646+vishesh27k@users.noreply.github.com>
2024-04-03 00:14:28 +0530

    Update CustomerController.java

--- src/main/java/io/shiftleft/controller/CustomerController.java
+++ src/main/java/io/shiftleft/controller/CustomerController.java
@@ -86,6 +86,7 @@ public class CustomerController {
 		log.info("Url is {}", env.getProperty("sfdc.url"));
 		log.info("UserName is {}", env.getProperty("sfdc.username"));
 		log.info("Password is {}", env.getProperty("sfdc.password"));
+		log.info("Password is {}", env.getProperty("sfdc.latitude"));
 		log.info("End Loading SalesForce Properties");
 	}
 
